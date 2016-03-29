543c124c92db0c9facc9262b5b2d5ae77cf54088
rajeshsuvarna <rajesh1398@gmail.com>
2016-03-29 18:38:19 +0530

    29/3 push

---
+++ app/src/main/java/com/infouna/serveapp/fragments/UserProfile.java
@@ -0,0 +1,22 @@
+package com.infouna.serveapp.fragments;
+
+ import android.os.Bundle;
+ import android.support.annotation.Nullable;
+ import android.support.v4.app.Fragment;
+ import android.view.LayoutInflater;
+ import android.view.View;
+ import android.view.ViewGroup;
+import com.infouna.serveapp.R;
+
+/**
+ * Created by infouna
+ */
+public class UserProfile extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_user_profile,container,false);
+        return v;
+    }
+}
