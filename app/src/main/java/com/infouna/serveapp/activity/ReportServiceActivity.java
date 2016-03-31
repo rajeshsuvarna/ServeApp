520708f56c7a98870d5fe843475ead3feaacf0e6
darshanjagthap <darshandjagthap@gmail.com>
2016-03-31 16:54:10 +0530

    frag_user_profile_30/3/2016(darsh)

---
+++ app/src/main/java/com/infouna/serveapp/activity/ReportServiceActivity.java
@@ -0,0 +1,17 @@
+package com.infouna.serveapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.infouna.serveapp.R;
+
+/**
+ * Created by Darshan on 31-03-2016.
+ */
+public class ReportServiceActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_service);
+    }
+}
