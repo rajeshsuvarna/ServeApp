acc5ceb251127a5c20cad65272d6435c549f37f9
darshanjagthap <darshandjagthap@gmail.com>
2016-04-04 16:35:11 +0530

    frag_user_profile_30/3/2016(darsh)

--- app/src/main/java/com/infouna/serveapp/activity/RateServiceActivity.java
+++ app/src/main/java/com/infouna/serveapp/activity/RateServiceActivity.java
@@ -1,17 +1,72 @@
 package com.infouna.serveapp.activity;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.infouna.serveapp.R;
+
+import butterknife.Bind;
 
 /**
  * Created by Darshan on 31-03-2016.
  */
 public class RateServiceActivity extends Activity {
+
+    ImageButton stars[] = new ImageButton[5];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_service);
+
+        stars[0] = (ImageButton) findViewById(R.id.star_1);
+        stars[1] = (ImageButton) findViewById(R.id.star_2);
+        stars[2] = (ImageButton) findViewById(R.id.star_3);
+        stars[3] = (ImageButton) findViewById(R.id.star_4);
+        stars[4] = (ImageButton) findViewById(R.id.star_5);
+
     }
+
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.star_1:
+                rater(1);
+                break;
+            case R.id.star_2:
+                rater(2);
+                break;
+            case R.id.star_3:
+                rater(3);
+                break;
+            case R.id.star_4:
+                rater(4);
+                break;
+            case R.id.star_5:
+                rater(5);
+                break;
+        }
+
+    }
+
+    public void rater(int rating)
+    {
+        clear_rating();
+        for(int i = 0;i < rating; i++)
+            stars[i].setImageResource(R.mipmap.ic_star_selected);
+    }
+
+    public void clear_rating()
+    {
+        for(int i = 0;i < 5; i++)
+            stars[i].setImageResource(R.mipmap.ic_star_deselected);
+    }
+
 }
--- app/src/main/java/com/infouna/serveapp/fragments/UserProfile.java
+++ app/src/main/java/com/infouna/serveapp/fragments/UserProfile.java
@@ -1,8 +1,11 @@
 package com.infouna.serveapp.fragments;
 
+ import android.content.Context;
+ import android.content.Intent;
  import android.os.Bundle;
  import android.support.annotation.Nullable;
  import android.support.v4.app.Fragment;
+ import android.support.v4.content.ContextCompat;
  import android.view.LayoutInflater;
  import android.view.View;
  import android.view.ViewGroup;
@@ -10,21 +13,30 @@ package com.infouna.serveapp.fragments;
  import android.widget.Toast;
 
  import com.infouna.serveapp.R;
+ import com.infouna.serveapp.activity.RateServiceActivity;
 
 /**
  * Created by infouna
  */
 public class UserProfile extends Fragment {
 
+    Context ctx;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_user_profile,container,false);
+        View v = inflater.inflate(R.layout.fragment_user_profile, container, false);
+         ctx = v.getContext();
 
         Button bt = (Button) v.findViewById(R.id.button_edit_my_profile);
 
-
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ctx, RateServiceActivity.class);
+                startActivity(i);
+            }
+        });
         return v;
     }
 }
