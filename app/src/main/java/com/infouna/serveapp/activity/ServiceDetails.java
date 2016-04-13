bc566f3315362ce96d19a6d05ce9e90db9d39dd9
darshanjagthap <darshandjagthap@gmail.com>
2016-04-13 16:15:36 +0530

    darshan_service details

--- app/src/main/java/com/infouna/serveapp/activity/ServiceDetails.java
+++ app/src/main/java/com/infouna/serveapp/activity/ServiceDetails.java
@@ -1,7 +1,17 @@
 package com.infouna.serveapp.activity;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.infouna.serveapp.R;
+
 /**
  * Created by Darshan on 13-04-2016.
  */
-public class ServiceDetails {
+public class ServiceDetails extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_details);
+    }
 }
