3eb306773c69b76aaa078ba04ecffca291b7a06d
darshanjagthap <darshandjagthap@gmail.com>
2016-04-20 16:30:31 +0530

    darshan_20_4_apr

--- app/src/main/java/com/infouna/serveapp/fragments/UserProfile.java
+++ app/src/main/java/com/infouna/serveapp/fragments/UserProfile.java
@@ -5,13 +5,10 @@ package com.infouna.serveapp.fragments;
  import android.os.Bundle;
  import android.support.annotation.Nullable;
  import android.support.v4.app.Fragment;
- import android.support.v4.content.ContextCompat;
  import android.view.LayoutInflater;
  import android.view.View;
  import android.view.ViewGroup;
  import android.widget.Button;
- import android.widget.Toast;
-
  import com.infouna.serveapp.R;
  import com.infouna.serveapp.activity.RateServiceActivity;
 
