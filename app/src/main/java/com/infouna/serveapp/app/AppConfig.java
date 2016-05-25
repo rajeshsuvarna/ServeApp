cb5ffb8dbf59ac9467286f5edba1686a4d482f7b
darshanjagthap <darshandjagthap@gmail.com>
2016-05-25 20:39:51 +0530

    darshan_20_4_apr

--- app/src/main/java/com/infouna/serveapp/app/AppConfig.java
+++ app/src/main/java/com/infouna/serveapp/app/AppConfig.java
@@ -1,9 +1,17 @@
 package com.infouna.serveapp.app;
 
 public class AppConfig {
+
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.0.102/android_login_api/login.php";
+    public static String URL_LOGIN = "serveapp.in/assets/api/login.php?key=fd0e5f476a68c73bba35f3ee71ff3b4a&mobile=";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.0.102/android_login_api/register.php";
+    public static String URL_REGISTER = " ";
+
+    // Dashboard services / categories home page
+    public static String URL_DASHBOARD_SERVICES_HOME = "http://serveapp.in/assets/api/getData.php?key=fd0e5f476a68c73bba35f3ee71ff3b4a&act=dashboard_services";
+
+    // Dashboard sub services
+    public static String URL_SUB_SEVICES_HOME = "serveapp.in/assets/api/getData.php?key=fd0e5f476a68c73bba35f3ee71ff3b4a&act=dashboard_sub_service&s_name=";
+
 }
